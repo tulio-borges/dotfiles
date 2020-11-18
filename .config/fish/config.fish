8333b2af0f63ba2cb7128849ad6504887035f54a
Tulio Cesar Borges <tulioborges11@gmail.com>
2020-11-18 20:56:28 -0300

    Add pyenv setup on config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,14 @@
 # ~/.config/fish/config.fish
 
+# env variables
+
 set -gx EDITOR vim
 set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
 set -gx BAT_THEME "Monokai Extended Light"
+
+# setting up pyenv
+
+status --is-interactive; and pyenv init - | source
+status --is-interactive; and pyenv virtualenv-init - | source
 
 starship init fish | source
